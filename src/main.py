988a75e1910bf0c26502f2383d317ef98d690181
will702 <67593974+will702@users.noreply.github.com>
2021-06-26 21:23:14 +0700

    Fix for android

--- src/main.py
+++ src/main.py
@@ -1,95 +1,146 @@
-# coding: utf8
-from __future__ import unicode_literals
-__version__ = '0.2'
+
+from kivy.app import App
+from kivy.lang import Builder
 
 
+from kivy.utils import platform
 
+from jnius import autoclass
 
+from oscpy.client import OSCClient
+from oscpy.server import OSCThreadServer
+from plyer import filechooser
 SERVICE_NAME = u'{packagename}.Service{servicename}'.format(
     packagename=u'org.kivy.oscservice',
     servicename=u'Pong'
+
 )
+KV = '''
+BoxLayout:
+    orientation: 'vertical'
+    BoxLayout:
+        size_hint_y: None
+        height: '30sp'
+        Button:
+            text: 'Turn On'
+            on_press: app.start_service()
+        Button:
+            text: 'Turn Off'
+            on_press: app.stop_service()
 
-# install_twisted_rector must be called before importing the reactor
+    ScrollView:
+        Label:
+            id: label
+            size_hint_y: None
+            height: self.texture_size[1]
+            text_size: self.size[0], None
 
+    BoxLayout:
+        size_hint_y: None
+        height: '30sp'
+        Button:
+            text: 'Play'
+            on_press: app.trying()
+        Button:
+            text: 'Clear'
+            on_press: label.text = ''
+        Label:
+            id: date
 
-
-from kivy.support import install_twisted_reactor
-
-install_twisted_reactor()
-
-# A Simple Client that send messages to the Echo Server
-from twisted.internet import reactor, protocol
-
-
-class EchoClient(protocol.Protocol):
-    def connectionMade(self):
-        self.factory.app.on_connection(self.transport)
-
-    def dataReceived(self, data):
-        self.factory.app.print_message(data.decode('utf-8'))
-
-
-class EchoClientFactory(protocol.ClientFactory):
-    protocol = EchoClient
-
-    def __init__(self, app):
-        self.app = app
-
-    def startedConnecting(self, connector):
-        self.app.print_message('Started to connect.')
-
-    def clientConnectionLost(self, connector, reason):
-        self.app.print_message('Lost connection.')
-
-    def clientConnectionFailed(self, connector, reason):
-        self.app.print_message('Connection failed.')
+'''
 
 
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.boxlayout import BoxLayout
-
-
-# A simple kivy App, with a textbox to enter messages, and
-# a large label to display all the messages received from
-# the server
-class TwistedClientApp(App):
-    connection = None
-    textbox = None
-    label = None
-
+class ClientServerApp(App):
     def build(self):
-        root = self.setup_gui()
-        self.connect_to_server()
-        return root
+        self.service = None
+        # self.start_service()
 
-    def setup_gui(self):
-        self.textbox = TextInput(size_hint_y=.1, multiline=False)
-        self.textbox.bind(on_text_validate=self.send_message)
-        self.label = Label(text='connecting...\n')
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(self.label)
-        layout.add_widget(self.textbox)
-        return layout
+        self.server = server = OSCThreadServer(encoding='utf-8')
+        server.listen(
+            address=b'localhost',
+            port=3002,
+            default=True,
+        )
 
-    def connect_to_server(self):
-        reactor.connectTCP('localhost', 8000, EchoClientFactory(self))
+        server.bind(b'/message', self.display_message)
+        server.bind(b'/date', self.date)
 
-    def on_connection(self, connection):
-        self.print_message("Connected successfully!")
-        self.connection = connection
+        self.client = OSCClient(b'localhost', 3000,encoding='utf-8')
+        self.root = Builder.load_string(KV)
+        return self.root
 
-    def send_message(self, *args):
-        msg = self.textbox.text
-        if msg and self.connection:
-            self.connection.write(msg.encode('utf-8'))
-            self.textbox.text = ""
+    def start_service(self):
+        if platform == 'android':
+            service = autoclass(SERVICE_NAME)
+            self.mActivity = autoclass(u'org.kivy.android.PythonActivity').mActivity
+            argument = ''
+            service.start(self.mActivity, argument)
+            self.service = service
 
-    def print_message(self, msg):
-        self.label.text += "{}\n".format(msg)
+        elif platform in ('linux', 'linux2', 'macosx', 'win'):
+            from runpy import run_path
+            from threading import Thread
+            self.service = Thread(
+                target=run_path,
+                args=['src/service.py'],
+                kwargs={'run_name': '__main__'},
+                daemon=True
+            )
+            self.service.start()
+        else:
+            raise NotImplementedError(
+                "service start not implemented on this platform"
+            )
+
+    def stop_service(self):
+        if self.service:
+            if platform == "android":
+                self.service.stop(self.mActivity)
+            elif platform in ('linux', 'linux2', 'macos', 'win'):
+                # The below method will not work.
+                # Need to develop a method like
+                # https://www.oreilly.com/library/view/python-cookbook/0596001673/ch06s03.html
+                self.service.stop()
+            else:
+                raise NotImplementedError(
+                    "service start not implemented on this platform"
+                )
+            self.service = None
+
+    def choose(self):
+        '''
+        Call plyer filechooser API to run a filechooser Activity.
+        '''
+        filechooser.open_file(on_selection=self.handle_selection)
+
+    def handle_selection(self, selection):
+        '''
+        Callback function for handling the selection response from Activity.
+        '''
+        self.selection = selection[0]
+
+        print(self.selection)
+
+        self.send(argumen=self.selection)
+
+    def send(self, *args, argumen):
+        self.client.send_message(b'/ping', [f'{argumen}'])
+    def trying(self,*args):
+        self.client.send_message(b'/my_function',['/sdcard/beep1.wav'])
+
+
+
+    def display_message(self, message):
+
+        if self.root:
+            self.root.ids.label.text += '{}\n'.format("Worked")
+
+    def date(self, message):
+
+        if self.root:
+            self.root.ids.date.text = message
 
 
 if __name__ == '__main__':
-    TwistedClientApp().run()
+    ClientServerApp().run()
+
